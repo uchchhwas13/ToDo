e887f5dcc61c1488340552c13cfbd456508d2296
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2023-03-14 11:39:17 +0600

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Uchchhwas Roy on 14/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
